2ec21c1cd4981869a95640d08554475a67972fed
MisterNay <misternay@odds.team>
2019-01-15 15:25:24 +0700

    Refector extract to method

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ func main() {
 func Fizzbuzz(input int) string {
 	if isFizzBuzz(input) {
 		return "FizzBuzz"
-	} else if input%3 == 0 {
+	} else if isFizz(input) {
 		return "Fizz"
-	} else if input%5 == 0 {
+	} else if isBuzz(input) {
 		return "Buzz"
 	} else {
 		return strconv.Itoa(input)
@@ -20,4 +20,11 @@ func Fizzbuzz(input int) string {
 
 func isFizzBuzz(input int) bool {
 	return input%3 == 0 && input%5 == 0
+}
+
+func isFizz(input int) bool {
+	return input%3 == 0
+}
+func isBuzz(input int) bool {
+	return input%5 == 0
 }
